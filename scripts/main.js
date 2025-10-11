73cd6684b1e70eabe57917fdb42e575fe51a11f8
robbieg2 <robbiesgraham2@gmail.com>
2025-10-11 14:50:53 +0100

    Adding Home Page

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,6 @@ import { redirectToSpotifyAuth, getAccessToken } from "./auth.js";
 const loginBtn = document.getElementById("login-btn");
 
 if (loginBtn) {
-	loginBtn.addEventListener("click"< redirectToSpotifyAuth);
+	loginBtn.addEventListener("click", redirectToSpotifyAuth);
 }
 
